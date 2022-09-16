13351afe75ef65d54b0f458d83ecb594d3e381e5
cmreavis <cmreavis97@gmail.com>
2022-09-16 15:02:42 -0400

    First commit for exercise 3

---
+++ MethodsWithLoopsAndConditionals/MethodsWithLoopsAndConditionals/Methods.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsWithLoopsAndConditionals
+{
+    internal class Methods
+    {
+        public static void Thousands()
+        {
+            for (int oneK = 1000; oneK >= -1000; oneK--)
+            {
+                Console.WriteLine(oneK);
+            }
+
+        }
+        public static void ByThrees()
+        {
+            for (int by3 = 3; by3 <= 999; by3 += 3)
+            {
+                Console.WriteLine(by3);
+            }
+
+        }
+        public static bool EqualOrNot(int a, int b)
+        {
+            if (a == b)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static void EvenOrOdd(int numberGiven)
+        {
+            if (numberGiven % 2 == 0)
+            {
+                Console.WriteLine($"Even!");
+            }
+            else
+            {
+                Console.WriteLine($"Odd!");
+            }
+        }
+        public static void PositiveNegative(int number)
+        {
+            if (number > 0)
+            {
+                Console.WriteLine($"That number is positive!");
+            }
+            else if (number == 0)
+            {
+                Console.WriteLine("Zero is neither positive nor negative!");
+            }
+            else
+            {
+                Console.WriteLine($"That number is negative!");
+            }
+        }
+        public static bool AbleToVote(int age)
+        {
+            if (age >= 18)
+            {
+                Console.WriteLine("You are of voting age!");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"You cannot vote yet! Give it {18 - age} years.");
+                return false;
+            }
+        }
+        public static bool BetweenTheseNums(int num)
+        {
+            if (num <= 10 && num >= -10)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static void Multiply()
+        {
+            Console.WriteLine("Input a number you would like to see multiplication tables for.");
+
+            var xNum = int.Parse(Console.ReadLine());
+            for (int x = 1; x <= 12; x++)
+            {
+                Console.WriteLine($"{xNum} * {x} = {xNum * x}.");
+            }
+        }   
+        
+            
+    }
+}
---
+++ MethodsWithLoopsAndConditionals/MethodsWithLoopsAndConditionals/Program.cs
@@ -0,0 +1,49 @@
+﻿
+namespace MethodsWithLoopsAndConditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                    //I have indented the instructions, simply uncomment the lines between for a demonstration of each method.
+
+                                        //LukeWarm Section
+
+                    //Write a method that will print all numbers 1000 through -1000
+            //Methods.Thousands();
+
+                    //Write a method that will print to the console numbers 3 through 999 by 3s
+            //Methods.ByThrees();
+
+                    //Write a method to accept two integers as parameters and check if they are equal
+            //Console.WriteLine(Methods.EqualOrNot(5, 5));
+
+                    //Write a method to check whether a given number is even or odd
+            //Methods.EvenOrOdd(2356783);
+
+                    //Write a method to check whether a given number is positive or negative
+            //Methods.PositiveNegative(-20);
+
+                    //Write a method to read the age of a candidate and determine whether they can vote
+            //Methods.AbleToVote(6);
+
+                                        //Heatin Up Section
+
+                    //Write a method to check if an integer (from the user) is in the range -10 to 10
+            //int numCheck;
+            //var userInput = "";
+            //    Console.WriteLine("Please input a number. I will check if it is between -10 and 10.");
+            //    userInput = Console.ReadLine();
+            //    Console.WriteLine();
+            //        int.TryParse(userInput, out numCheck);
+            //        Console.WriteLine(Methods.BetweenTheseNums(numCheck));
+
+                //Write a method to display the multiplication table (from 1 to 12) of a given integer
+            //Methods.Multiply();
+
+        }
+    }
+}
+
+
+
