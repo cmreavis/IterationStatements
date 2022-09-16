d624dcc09e6c90fb3005da495ffe5ef01ed69718
cmreavis <cmreavis97@gmail.com>
2022-09-16 11:19:34 -0400

    Added exercise 1

---
+++ 1exerciseforloop/1exerciseforloop/Program.cs
@@ -0,0 +1,14 @@
+﻿for (int x = 1; x < 16; x++)
+{
+    Console.WriteLine($"{x}");
+}
+//for (int x = 100; x < 130; x += 2)
+//{
+//    Console.WriteLine($"{x}");
+//}
+//for (int x = 1000; x < 1001; x++)
+//{
+//    Console.WriteLine($"{x}");
+//}
+
+
